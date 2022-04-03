fec5c201eb4ef2069f0d3aad17ed13993c09b00f
Tomek Jaworski <tj.toja@gmail.com>
2022-04-03 19:59:26 +0200

    Created application skeleton

---
+++ src/main/java/com/infoshareacademy/responsibledrinkersweb/ResponsibleDrinkersWebApplication.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.responsibledrinkersweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponsibleDrinkersWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponsibleDrinkersWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infoshareacademy/responsibledrinkersweb/ResponsibleDrinkersWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infoshareacademy.responsibledrinkersweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponsibleDrinkersWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
