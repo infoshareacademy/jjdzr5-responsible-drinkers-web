98bdd0310488854f55c5c07cce92d025c8c711e0
Tomek Jaworski <tj.toja@gmail.com>
2022-04-05 15:54:34 +0200

    Added date formatter to html

--- src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
+++ src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.infoshareacademy.responsibledrinkersweb.controller;
 
+import com.infoshareacademy.responsibledrinkersweb.sevice.DateFormat;
 import com.infoshareacademy.responsibledrinkersweb.sevice.DrinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,10 +12,14 @@ public class IndexController {
 
     @Autowired
     private DrinkService drinkService;
+    @Autowired
+    private DateFormat dateFormat;
 
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("newestdrinks", drinkService.getNewest(8));
+        model.addAttribute("dateformat", dateFormat.getDatePatter());
+
         return "index";
     }
 }
