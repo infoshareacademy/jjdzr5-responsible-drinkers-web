5ec3e6b65ef7682db9399764f6b05b51b47bcd9c
Shirley Sopotnicka <shirley.sopotnicka@gmail.com>
2022-04-08 17:23:04 +0200

    Changed href to th:href on all subpages, romoved '.html' at the end of address

--- src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
+++ src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
@@ -24,41 +24,41 @@ public class IndexController {
         return "index";
     }
 
-    @RequestMapping("/index.html")
+    @RequestMapping("/index")
     public String index(Model model) {
         model.addAttribute("newestdrinks", drinkService.getNewest(ELEMENTS_TO_PRINT));
         model.addAttribute("dateformat", dateFormat.getDatePatter());
         return "index";
     }
 
-    @RequestMapping("/drink_list.html")
+    @RequestMapping("/drink_list")
     public String drinkList(Model model) {
         model.addAttribute("drinklist",drinkService.getDrinks());
         model.addAttribute("dateformat", dateFormat.getDatePatter());
         return "drink_list";
     }
 
-    @RequestMapping("/add_new_drink.html")
+    @RequestMapping("/add_new_drink")
     public String addDrink(Model model) {
         return "add_new_drink";
     }
 
-    @RequestMapping("/manager.html")
+    @RequestMapping("/manager")
     public String manager(Model model) {
         return "manager";
     }
 
-    @RequestMapping("/login.html")
+    @RequestMapping("/login")
     public String login(Model model) {
         return "login";
     }
 
-    @RequestMapping("/signup.html")
+    @RequestMapping("/signup")
     public String signup(Model model) {
         return "signup";
     }
 
-    @RequestMapping("/account_settings.html")
+    @RequestMapping("/account_settings")
     public String account(Model model) {
         return "account_settings";
     }
