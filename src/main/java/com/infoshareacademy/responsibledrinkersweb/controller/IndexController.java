fca8e3f6291b6519748f358d7be80a7d120041c0
Tomek Jaworski <tj.toja@gmail.com>
2022-04-09 11:17:34 +0200

    Implemented dorting by attribute

--- src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
+++ src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
@@ -1,11 +1,18 @@
 package com.infoshareacademy.responsibledrinkersweb.controller;
 
+import com.infoshareacademy.drinkers.domain.drink.Drink;
+import com.infoshareacademy.drinkers.service.sorting.SortDrinks;
+import com.infoshareacademy.drinkers.service.sorting.SortItems;
 import com.infoshareacademy.responsibledrinkersweb.sevice.DateFormat;
 import com.infoshareacademy.responsibledrinkersweb.sevice.DrinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -32,8 +39,20 @@ public class IndexController {
     }
 
     @RequestMapping("/drink_list")
-    public String drinkList(Model model) {
-        model.addAttribute("drinklist",drinkService.getDrinks());
+    public String drinkList(Model model, @RequestParam int sort) {
+        List<Drink> drinkList = drinkService.getDrinks();
+        SortDrinks sortDrinks = new SortDrinks(drinkList);
+        if (sort == 1) {
+            model.addAttribute("drinklist", sortDrinks.getSortedList(SortItems.ID));
+        } else if (sort == 2) {
+            model.addAttribute("drinklist", sortDrinks.getSortedList(SortItems.DRINK_NAME));
+        } else if (sort == 4) {
+            model.addAttribute("drinklist", sortDrinks.getSortedList(SortItems.ALCOHOLIC));
+        } else if (sort == 3) {
+            model.addAttribute("drinklist", sortDrinks.getSortedList(SortItems.DATE));
+        } else {
+            model.addAttribute("drinklist", drinkList);
+        }
         model.addAttribute("dateformat", dateFormat.getDatePatter());
         return "drink_list";
     }
