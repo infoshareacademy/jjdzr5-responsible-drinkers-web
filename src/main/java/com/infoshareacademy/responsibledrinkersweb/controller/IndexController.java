f42df70ee4b0bb3a0e5096caa3cc9ec865813702
Tomek Jaworski <tj.toja@gmail.com>
2022-04-10 20:36:33 +0200

    Updated controllers

--- src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
+++ src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
@@ -8,8 +8,7 @@ import com.infoshareacademy.responsibledrinkersweb.sevice.DrinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -24,21 +23,21 @@ public class IndexController {
 
     private static final Integer ELEMENTS_TO_PRINT = 8;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String main(Model model) {
         model.addAttribute("newestdrinks", drinkService.getNewest(ELEMENTS_TO_PRINT));
         model.addAttribute("dateformat", dateFormat.getDatePatter());
         return "index";
     }
 
-    @RequestMapping("/index")
+    @GetMapping("/index")
     public String index(Model model) {
         model.addAttribute("newestdrinks", drinkService.getNewest(ELEMENTS_TO_PRINT));
         model.addAttribute("dateformat", dateFormat.getDatePatter());
         return "index";
     }
 
-    @RequestMapping("/drink_list")
+    @GetMapping("/drink_list")
     public String drinkList(Model model, @RequestParam int sort) {
         List<Drink> drinkList = drinkService.getDrinks();
         SortDrinks sortDrinks = new SortDrinks(drinkList);
@@ -57,27 +56,28 @@ public class IndexController {
         return "drink_list";
     }
 
-    @RequestMapping("/add_new_drink")
-    public String addDrink(Model model) {
+    @GetMapping("/add_new_drink")
+    public String addDrink(Model model, Drink drink) {
+        model.addAttribute("drink", new Drink());
         return "add_new_drink";
     }
 
-    @RequestMapping("/manager")
+    @GetMapping("/manager")
     public String manager(Model model) {
         return "manager";
     }
 
-    @RequestMapping("/login")
+    @GetMapping("/login")
     public String login(Model model) {
         return "login";
     }
 
-    @RequestMapping("/signup")
+    @GetMapping("/signup")
     public String signup(Model model) {
         return "signup";
     }
 
-    @RequestMapping("/account_settings")
+    @GetMapping("/account_settings")
     public String account(Model model) {
         return "account_settings";
     }
