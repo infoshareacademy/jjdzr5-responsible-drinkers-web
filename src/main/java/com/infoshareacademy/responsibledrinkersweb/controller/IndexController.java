c61c5090749abaae4762baeb9e811cf6453f22a7
Tomek Jaworski <tj.toja@gmai.com>
2022-04-11 09:12:19 +0200

    Added form & controller

--- src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
+++ src/main/java/com/infoshareacademy/responsibledrinkersweb/controller/IndexController.java
@@ -1,10 +1,14 @@
 package com.infoshareacademy.responsibledrinkersweb.controller;
 
+import com.infoshareacademy.drinkers.domain.drink.Drink;
+import com.infoshareacademy.drinkers.domain.drink.DrinkBuilder;
 import com.infoshareacademy.responsibledrinkersweb.sevice.DateFormat;
 import com.infoshareacademy.responsibledrinkersweb.sevice.DrinkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -33,14 +37,22 @@ public class IndexController {
 
     @RequestMapping("/drink_list.html")
     public String drinkList(Model model) {
-        model.addAttribute("drinklist",drinkService.getDrinks());
+        model.addAttribute("drinklist", drinkService.getDrinks());
         model.addAttribute("dateformat", dateFormat.getDatePatter());
         return "drink_list";
     }
 
     @RequestMapping("/add_new_drink.html")
-    public String addDrink(Model model) {
+    public String addNewDrink(Model model, Drink drink) {
+        model.addAttribute("drink", new Drink());
         return "add_new_drink";
+    }
+
+    @PostMapping("/new_drink")
+    public String newDrink(@ModelAttribute Drink drink, Model model) {
+
+        model.addAttribute("drink", drink);
+        return "new_drink";
     }
 
     @RequestMapping("/manager.html")
