167d6487a61ddf9115d0e7f94b367511e76274ba
Tomek Jaworski <tj.toja@gmail.com>
2022-04-25 20:19:53 +0200

    Implemented class to verify URL

--- src/main/java/com/infoshareacademy/responsibledrinkersweb/service/DrinkService.java
+++ src/main/java/com/infoshareacademy/responsibledrinkersweb/service/DrinkService.java
@@ -3,9 +3,11 @@ package com.infoshareacademy.responsibledrinkersweb.service;
 import com.infoshareacademy.drinkers.domain.drink.Alcoholic;
 import com.infoshareacademy.drinkers.domain.drink.Drink;
 import com.infoshareacademy.drinkers.service.manage.DrinkManager;
+import com.infoshareacademy.responsibledrinkersweb.exceptions.ImageNotFound;
 import com.infoshareacademy.responsibledrinkersweb.repository.DrinkRepository;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -29,6 +31,8 @@ public class DrinkService {
 
     public void addDrink(Drink drink) {
         DrinkManager drinkManager = new DrinkManager(getDrinks());
+        String url = ImageNotFound.checkImageURL(drink.getDrinkThumb().toString());
+        drink.setDrinkThumb(URI.create(url));
         if (drink.getIngredient3().isBlank()) {
             drink.setIngredient3(null);
         }
