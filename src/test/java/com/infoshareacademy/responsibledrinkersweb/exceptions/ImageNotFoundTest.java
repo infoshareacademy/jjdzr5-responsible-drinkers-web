f0328bbf3c48fa0f81547995776a6eaac0775197
Tomek Jaworski <tj.toja@gmail.com>
2022-05-15 19:43:48 +0200

    Initial commit

---
+++ src/test/java/com/infoshareacademy/responsibledrinkersweb/exceptions/ImageNotFoundTest.java
@@ -0,0 +1,12 @@
+package com.infoshareacademy.responsibledrinkersweb.exceptions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageNotFoundTest {
+
+    @Test
+    void verifyURL() {
+    }
+}
