15590dd0579a3e96fdd9bf9d87f56d92c03ff6f7
Tomek Jaworski <tj.toja@gmail.com>
2022-05-17 17:30:35 +0200

    Code cleanuped

--- src/main/java/com/infoshareacademy/responsibledrinkersweb/config/WebSecurityConfig.java
+++ src/main/java/com/infoshareacademy/responsibledrinkersweb/config/WebSecurityConfig.java
@@ -4,19 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -62,6 +55,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .deleteCookies("JSESSIONID");
     }
-
 
 }
