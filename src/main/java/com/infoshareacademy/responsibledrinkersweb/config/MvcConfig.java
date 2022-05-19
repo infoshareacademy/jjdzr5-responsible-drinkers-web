8c11800d6ad8fd2c96ea85abbe21dcd13f7702ee
Shirley Sopotnicka <shirley.sopotnicka@gmail.com>
2022-05-19 13:51:25 +0200

    changed implementation of in-memory users

--- src/main/java/com/infoshareacademy/responsibledrinkersweb/config/MvcConfig.java
+++ src/main/java/com/infoshareacademy/responsibledrinkersweb/config/MvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
